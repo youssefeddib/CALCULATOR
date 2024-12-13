2305e4413ba1cca6759c25c2b82dc8aed1f811dd
youssef eddib <eddiby79@gmail.com>
2024-12-13 09:29:37 +0100

    quiter

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -14,8 +13,9 @@ public class Main {
             System.out.println("3. Multiplication");
             System.out.println("4. Division");
             System.out.println("5. Puissance");
-            System.out.println("6.Racine carrée");
-            System.out.println("7.Factorielle");
+            System.out.println("6. Racine carrée");
+            System.out.println("7. Factorielle");
+            System.out.println("8. Quiter");
 
             System.out.print("Choisissez une opération: ");
             int choix = scanner.nextInt();
@@ -70,6 +70,8 @@ public class Main {
                     num1 = scanner.nextDouble();
                     Factorielle(num1);
                     break;
+                case 8 :
+                    System.out.print("au revoir !!!: ");
                 default:
                     System.out.println("Choix invalide !!");
             }
